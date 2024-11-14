b0feddfc4f71e4db97e4dfd4746c75613e61bc7c
Agarwalvidu <119511621+Agarwalvidu@users.noreply.github.com>
2024-11-14 18:08:33 +0530

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+// import Navbar from '../components/Navbar/Navbar'; 
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Homepage/> */}
+    </div>
+  );
+};
+
+export default Home;
