a6b3b0440d051c7f953c29352e132dd964dcbc6f
Agarwalvidu <119511621+Agarwalvidu@users.noreply.github.com>
2024-11-14 19:04:24 +0530

    basic homepage added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import './static/css/bootstrap.css';
+import './static/css/style.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './pages/home';
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
         </Routes>
     </Router>
   );
